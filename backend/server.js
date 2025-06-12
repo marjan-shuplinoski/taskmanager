ad028a1675ca4547f4f9996ccf4ade15ba428048
marjan-shuplinoski <marjan@shuplinoski.com>
2025-06-12 12:00:14 +0200

    minor bugs

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
-
 // Use the new routes file for /api/v1 endpoints
 app.use('/api/v1', routes);
 
